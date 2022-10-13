ae3ff0ba194ea2cd679580cb26ee43dc3ec21157
QuotheRavn <99696610+QuotheRavn@users.noreply.github.com>
2022-10-12 22:25:37 -0500

    popup close box added, partial functionality;list overlap of video fixed

--- src/player/Controller.java
+++ src/player/Controller.java
@@ -1,5 +1,6 @@
 package player;
 
+import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
@@ -10,7 +11,9 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
@@ -19,6 +22,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
@@ -29,21 +33,25 @@ import javafx.util.Duration;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.File;
 import java.net.URL;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.Timer;
+
 
 /**
  * Class to manipulate and utilize the FXML GUI.
  */
 public class Controller implements Initializable {
 
-    //The main parent container
+    //The main window
+	@FXML
+	BorderPane window;
+	
+	//The main parent container
     @FXML
     private VBox vboxParent;
 
@@ -127,6 +135,8 @@ public class Controller implements Initializable {
         mediaPlayer = new MediaPlayer(mediaFile);
         mediaPlayer.setAutoPlay(true);
         mediaView.setMediaPlayer(mediaPlayer);
+        mediaView.fitHeightProperty().bind(vboxParent.heightProperty());
+        mediaView.fitWidthProperty().bind(vboxParent.widthProperty());
         mediaPlayer.play();
 
         //Setup initial button Images and defaults
@@ -137,15 +147,49 @@ public class Controller implements Initializable {
 			client = new Client(socketfact);
 		}catch(Exception e) {
 			e.printStackTrace();
-			System.out.println("Error creating client.");
+			System.out.println("Error creating initial client.");
 		}    	
 
     	//request list of available media
-        //DP: This will run the call every 30 seconds
+        //This will run the call every 30 seconds
         ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
         executor.scheduleAtFixedRate(() -> client.receiveListFromServer(mediaList), 0, 30, TimeUnit.SECONDS);
+      
+        /**
+         * Handle closing the window
+         * Close all items
+         * Close server connection. 
+         * Clear Cache
+         */
+        Stage closeStage = Main.getPrimaryStage();
+        	closeStage.setOnCloseRequest( e -> { 
+            	 e.consume();
+            	 Alert a = 
+            	            new Alert(Alert.AlertType.CONFIRMATION, 
+            	                    "Are you sure you want to quit?", 
+            	                    ButtonType.YES,
+            	                    ButtonType.NO);
+            	      Optional<ButtonType> confirm = a.showAndWait();
+            	      File directory = new File(CACHE);
+            	      if (confirm.isPresent() && confirm.get() == ButtonType.YES) {           	    	  
+            	    	  //closes media player
+            	    	  mediaPlayer.dispose();
+            	    	  //closes refresh thread
+            	    	  executor.close();
+            	    	  //closes window
+            	    	  closeStage.close();
+            	          for (File file: Objects.requireNonNull(directory.listFiles())) {
+            	              if (!file.isDirectory()) {
+            	                  file.delete();
+            	              }
+            	          }
+            	    	  //CONNECTIONS NOT CLOSED
+            	          
+            	    	  //Platform.exit();
+            	    	  
+            	      }
+            }  );
         
-    	
         
         /**
          * Play Button functionality
@@ -336,6 +380,7 @@ public class Controller implements Initializable {
                 }
             }
         });
+        
 
         // Work with the full screen label.
         labelFullScreen.setOnMouseClicked(new EventHandler<MouseEvent>() {
